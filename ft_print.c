cb5e53fa37841b25b9251c4b339ac607621191b2
Constance <--global>
2023-04-29 03:57:55 +0200

    first aux function

--- ft_print.c
+++ ft_print.c
@@ -6,11 +6,22 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 03:23:42 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/29 03:33:17 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/04/29 03:57:27 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lbft.h"
 #include "printf.h"
 
-int	
+int	ft_len(uintptr_t num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		len++;
+		num = num /16;
+	}
+	return (len);
+}
