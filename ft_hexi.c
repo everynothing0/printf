8ee6ddd18c34548b863067f108482273a73d9934
Constance <--global>
2023-04-30 04:38:27 +0200

    a revoir

--- ft_hexi.c
+++ ft_hexi.c
@@ -6,7 +6,7 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 02:47:23 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/30 02:55:35 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/04/30 04:37:21 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,35 @@ int	ft_lenhexi(unsigned num) // int len
 	return (len);
 }
 
-int	ft_hexid(unsigned num, const char conv)
+void	ft_hexid(unsigned num, const char conv)
 {
+	if (num >= 16)
+	{
+		ft_hexi(num / 16, conv);
+		ft_hexi(num % 16, conv);
+	}
+	else
+	{
+		if (num >= 9)
+			ft_putchar_fd((num + '0'), 1);
+		else
+		{
+			if (conv == 'x')
+				ft_putchar_fd((num - 10 + 'a'), 1);
+			if (conv == 'X')
+				ft_putchar_fd((num - 10 + 'A'), 1);
+		}
+	}
+}
 
+int	print_hexi(unsigned int num, const char conv)
+{
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return (0);
+	}
+	else
+		ft_hexid(num, conv);
+	return (ft_lenhexi(num));
 }
