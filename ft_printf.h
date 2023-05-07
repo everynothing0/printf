51cbd2d632fd58ad34bff9ab13e1c23caef85d76
Constance <--global>
2023-05-07 23:43:53 +0200

    printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 00:39:35 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/05/07 00:44:30 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/05/07 23:43:06 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,16 @@
 # include <stddef.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 
 int		ft_printf(const char *str, ...);
 void	ft_adress(void *num, int *cnt);
 void	ft_putchar(int c, int *cnt);
 void	ft_hexmaj(unsigned int num, int *cnt);
 void	ft_hexmin(unsigned int num, int *cnt);
+void	ft_printf(const char *str, ...);
+void	ft_putnbr(int nb, int *cnt);
+void	ft_putnbru(unsigned int nb, int *cnt);
+void	ft_putstr(char *str, int *cnt);
 
 #endif
