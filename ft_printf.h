3f1358333168adb8a33bf5e66a5584c12c527c56
Constance De Voogd <cde-voog@c2r10p1.42lehavre.fr>
2023-05-14 12:22:05 +0200

    HHHHHHHHH

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 00:39:35 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/05/07 23:43:06 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/05/07 19:36:31 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,15 @@
 # include <stddef.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <limits.h>
 
 int		ft_printf(const char *str, ...);
 void	ft_adress(void *num, int *cnt);
 void	ft_putchar(int c, int *cnt);
 void	ft_hexmaj(unsigned int num, int *cnt);
 void	ft_hexmin(unsigned int num, int *cnt);
-void	ft_printf(const char *str, ...);
+void	ft_putchar(int c, int *cnt);
 void	ft_putnbr(int nb, int *cnt);
-void	ft_putnbru(unsigned int nb, int *cnt);
+void	ft_putnbru(unsigned int num, int *cnt);
 void	ft_putstr(char *str, int *cnt);
 
 #endif
